09a3368cd034baf82b31b362b13b2c872323d054
Frank Desmettre <frank.desmettre@gmail.com>
2016-09-16 22:43:29 +0200

    Add silence

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const mic = require('mic');
+const fs = require('fs');
+
+const micInstance = mic();
+const micInputStream = micInstance.getAudioStream();
+
+micInstance.start();
+
+function decibelsToGain(value) {
+  if (value <= -40) {
+    return 0
+  }
+  return Math.round(Math.exp(value / 8.6858) * 10000) / 10000
+}
+
+const VUMeter = require('vu-meter');
+const meter = new VUMeter()
+micInputStream.pipe(meter)
+
+meter.on('data', (data) => {
+  const dB = data[0];
+  const gain = decibelsToGain(dB);
+  if (gain > 0.4) {
+    playSilence();
+  }
+});
+
+const player = require('play-sound')();
+
+
+let playing = false;
+function playSilence() {
+  if (playing) return;
+
+  player.play('silence.mp3', function(err){})
+  playing = true;
+  setTimeout(() => {
+    playing = false;
+  }, 2500);
+}
